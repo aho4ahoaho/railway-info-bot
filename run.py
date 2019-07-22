1992e52f21b22a44706c13657ad4b2fefcc6073e
aho4ahoaho <aho4ahoaho@users.noreply.github.com>
2019-07-22 14:49:09 +0900

    Add files via upload

--- run.py
+++ run.py
@@ -1,59 +1,67 @@
-from slacker import Slacker
+from slackclient import SlackClient
 import json
 import requests
 import datetime
 import time
+import os
 
+client=SlackClient(os.environ["SLACK_TOKEN"])
 
+def channels_list():
+    global channels
+    json_data = client.api_call("channels.list")
+    channels = json_data['channels']
 
-slack = Slacker("<YOUR-SLACK-TOKEN>")
-
+def railinfo():
+    global info
+    res = requests.get("http://aho4ahoaho.main.jp/railway-info/index.php")
+    info = json.loads(str(res.text))
 
 def message():
-    json_data = json.loads(str(slack.channels.list()))
-    channels = json_data['channels']
-    res = requests.get("./index.php")
-    info = json.loads(str(res.text))
-    post_list = str()
+    channels_list()
+    railinfo()
+    post_list=str()
     for channel in channels:
         if channel["is_member"]:
+            send="#"+channel["name"]
             post_list = post_list+"{0}".format(channel["name"])+","
-            slack.chat.post_message(
-                "#{0}".format(channel["name"]),
-                username="鉄道運行情報bot",
-                icon_url="./icon.php",
-                attachments=[{
+            client.api_call("chat.postMessage",
+            channel="#"+channel["name"],
+            username="鉄道運行情報bot",
+            icon_url="http://aho4ahoaho.main.jp/railway-info/icon",
+            text="{0}時{1}分現在の運行情報です。".format(datetime.datetime.now().hour,datetime.datetime.now().minute),
+            attachments=[{
                     "pretext":"北海道",
                     "text":"{}".format(info["Hokaido"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/2/"),
-                    "color":"FF9900",
+                    "color":"fc6efd",
                 },{
                     "pretext":"東北",
                     "text":"{}".format(info["Tohoku"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/3/"),
-                    "color":"FF9900",
+                    "color":"a54ec9",
                 },{
                     "pretext":"関東",
                     "text":"{}".format(info["Kanto"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/4/"),
-                    "color":"FF9900",
+                    "color":"e472b0",
                 },{
                     "pretext":"中部",
                     "text":"{}".format(info["Chubu"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/5/"),
-                    "color":"FF9900",
+                    "color":"541cbd",
                 },{
                     "pretext":"近畿",
                     "text":"{}".format(info["Kinki"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/6/"),
-                    "color":"FF9900",
+                    "color":"ee25cb",
                 },{
                     "pretext":"中国",
                     "text":"{}".format(info["Chugoku"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/8/"),
-                    "color":"FF9900",
+                    "color":"fa5b00",
                 },{
                     "pretext":"四国",
                     "text":"{}".format(info["Shikoku"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/9/"),
-                    "color":"FF9900",
+                    "color":"5a8d02",
                 },{
                     "pretext":"九州",
                     "text":"{}".format(info["Kyushu"].replace(",","\n")+"\nhttps://transit.yahoo.co.jp/traininfo/area/7/"),
-                    "color":"FF9900",
+                    "color":"81b040",
                 }]
             )
 
@@ -69,7 +77,7 @@ while True:
     if date.hour == 16 and date.minute == 00 :
         message()
         print("16時間のスリープに入ります")
-        print(57000)
+        time.sleep(57000)
 
     print("29秒後にもう一度ループします。"+str(date))
     time.sleep(29)
